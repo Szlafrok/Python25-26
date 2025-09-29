cf81f5a3891823f8c922ab9152ffb9fa14d1563e
PiotrekP <kamiennygigant@gmail.com>
2025-09-29 15:41:18 +0200

    aktualizacja kodu 15:40

--- Lekcja02/pokaz.py
+++ Lekcja02/pokaz.py
@@ -12,3 +12,28 @@ print(zmienna)
 zmienna_2 = "Kociak"
 
 print(zmienna_2)
+
+kociak = "koteł"
+
+zmienna_3 = kociak
+print(zmienna_3)
+
+
+calkowita = 18
+dziesietna = 12.4
+tekst = "słowo"
+prawda = True
+falsz = False
+
+print(type(calkowita)) # int (integer)
+print(type(dziesietna)) # float (liczba zmiennoprzecinkowa)
+print(type(tekst)) # str (string)
+print(type(prawda)) # bool (boolean)
+
+print("------------")
+tekst = calkowita
+print(type(tekst))
+
+prawda = falsz
+print(type(prawda))
+
