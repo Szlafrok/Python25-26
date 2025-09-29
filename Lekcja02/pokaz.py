9271013472b959ac7d57e1e9cdd37ff1ab1a04d0
PiotrekP <kamiennygigant@gmail.com>
2025-09-29 15:21:13 +0200

    aktualizacja lekcja 2

---
+++ Lekcja02/pokaz.py
@@ -0,0 +1,14 @@
+# Co to jest komentarz?
+"""
+A tutaj mam taki fajny komentarz
+Taki wielolinijkowy
+"""
+
+zmienna = 15
+
+print("zmienna")
+print(zmienna)
+
+zmienna_2 = "Kociak"
+
+print(zmienna_2)
