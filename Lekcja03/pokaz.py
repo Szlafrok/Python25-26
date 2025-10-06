f7b0a411a99e40d3aac7dfa6d2ad43715aca4f88
PiotrekP <kamiennygigant@gmail.com>
2025-10-06 15:33:19 +0200

    aktualizacja L3 15:33

---
+++ Lekcja03/pokaz.py
@@ -0,0 +1,31 @@
+#imie = input("Wprowadź imię: ")
+#nazwisko = input("Wprowadź nazwisko: ")
+
+#print(imie, nazwisko)
+#wiek = int(input("Wprowadź wiek: "))
+
+a = 12
+b = 5
+
+print(a + b) # 12 + 5 =17
+print(a - b) # 12 - 5 = 7
+print(a * b) # 12 * 5 = 60
+print(a / b) # 12 / 5 = 2.4 -> ZAWSZE DAJE FLOAT
+
+c = 2 * a + b # 2 * 12 + 5 = 24 + 5 = 29
+print(c)
+
+print(-a) # -12
+
+print(a // b) # 2 -> # ZAWSZE DAJE INT
+
+# Operacje int-int, int-float, float-float
+print(2 + 2)
+print(2 + 2.5)
+print(2.5 + 2.5)
+
+# Potęgowanie
+print(a ** b) # a - podstawa, b - wykładnik. 248832
+
+# Dzielenie z resztą (modulo)
+print(a % b) # 12 % 5 = 
