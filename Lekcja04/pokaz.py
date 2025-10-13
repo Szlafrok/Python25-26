05f799a899fbcbed137da0c69925a5c5c09109fc
PiotrekP <kamiennygigant@gmail.com>
2025-10-13 16:04:53 +0200

    L4 - Rollercoaster

--- Lekcja04/pokaz.py
+++ Lekcja04/pokaz.py
@@ -50,16 +50,24 @@ print(250 <= 300) # True
 #print(250 >= "250") # BŁĄD
 print(250 <= 250.0) # True
 
-# --- ZADANIE SAMODZIELNE ---
+# --- ĆWICZENIE ---
+print(12 > 15) # Fałsz // >, ==, >=
+print(5 < 15000) # Prawda 
+print(120 != 120) # Fałsz
+print(60 == 15) # Fałsz
+print(25.3421 == 25.3421) # Prawda
 
 # Wypisz w konsoli zasadę działania programu
 # print("Czy możesz skorzystać z roller-coastera?")
 # print("True - możesz skorzystać z roller-coastera")
 # print("False - nie możesz skorzystać z roller-coastera")
-
+print("--- Rollercoaster --- ")
 # Zapytaj użytkownika o jego wzrost - pamiętaj o konwersji odpowiedzi do typu int
 # Domyślnie odpowiedź użytkownika jest tekstem
+wzrost_cm = int(input("Podaj swój wzrost w cm: "))
 
 # Sprawdź wynik działania logicznego i zapisz go do zmiennej "werdykt"
+werdykt = wzrost_cm >= 150
 
 # Wyświetl zawartość zmiennej werdykt
+print(werdykt)
