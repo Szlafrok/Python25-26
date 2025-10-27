e5a2e8837c04226db56181f792753e66dd5ebefa
PiotrekP <kamiennygigant@gmail.com>
2025-10-27 17:08:53 +0100

    po lekcji 6

--- Lekcja06/srednia_ocen.py
+++ Lekcja06/srednia_ocen.py
@@ -4,5 +4,18 @@
 # - wczyta oceny z: matmy, polskiego, historii, wf
 
 # i określi, czy otrzymamy pasek na świadectwie. Warunki paska:
-# - średnia co najmniej 4.75
+# - średnia co najmniej 4.75 (suma ocen / liczba ocen)
 # - zachowanie co najmniej bardzo dobre (5)
+
+zachowanie = int(input("Podaj ocenę z zachowania"))
+matma = int(input("Podaj ocenę z matmy"))
+polski = int(input("Podaj ocenę z polskiego"))
+historia = int(input("Podaj ocenę z historii"))
+wf = int(input("Podaj ocenę z wf-u"))
+
+srednia = (matma + polski + historia + wf) / 4
+
+if srednia >= 4.75 and zachowanie >= 5:
+    print("Masz pasek!")
+else:
+    print("Nie masz paska!")
