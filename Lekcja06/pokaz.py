8ac3ad155957a8ed5a8e6d2e385f82c3619dea2c
PiotrekP <kamiennygigant@gmail.com>
2025-10-27 16:06:04 +0100

    zadanie samodzielne

--- Lekcja06/pokaz.py
+++ Lekcja06/pokaz.py
@@ -8,27 +8,48 @@ else:
     print("Nie jest to ani chomik ani kot ani orka!")
 
 
-print("---- KALKULATOR ----")
-a = float(input("Podaj pierwszą liczbę: "))
-b = float(input("Podaj drugą liczbę: "))
+# print("---- KALKULATOR ----")
+# a = float(input("Podaj pierwszą liczbę: "))
+# b = float(input("Podaj drugą liczbę: "))
 
-print("+ -> Dodawanie") 
-print("- -> Odejmowanie")
-print("* -> Mnożenie")
-print("/ -> Dzielenie")
+# print("+ -> Dodawanie") 
+# print("- -> Odejmowanie")
+# print("* -> Mnożenie")
+# print("/ -> Dzielenie")
 
-operacja = input("Podaj operator: ")
+# operacja = input("Podaj operator: ")
 
-if operacja == "+":
-    print(f"{a} + {b} = {a + b}")
-elif operacja == "-":
-    print(f"{a} - {b} = {a - b}")
-elif operacja == "*":
-    print(f"{a} * {b} = {a * b}")
-elif operacja == "/":
-    if b == 0:
-        print("Dzielenie przez 0 jest niemożliwe!")
-    else:
-        print(f"{a} / {b} = {a / b}")
+# if operacja == "+":
+#     print(f"{a} + {b} = {a + b}")
+# elif operacja == "-":
+#     print(f"{a} - {b} = {a - b}")
+# elif operacja == "*":
+#     print(f"{a} * {b} = {a * b}")
+# elif operacja == "/":
+#     if b == 0:
+#         print("Dzielenie przez 0 jest niemożliwe!")
+#     else:
+#         print(f"{a} / {b} = {a / b}")
+# else:
+#     print("Operator nie został rozpoznany!")
+
+
+
+# -----------------------
+# Czy dane słowo zawiera?
+print("------- SŁOWO ZAWIERA -------")
+
+tekst = "Kaczka zabiła rondlem borsuka z kebabem"
+
+if "czk" in tekst and "rondla" in tekst:
+    print("Zawiera taki podciąg!")
 else:
-    print("Operator nie został rozpoznany!")
+    print("Nie zawiera takiego podciągu!")
+
+
+# ----- ZADANIE SAMODZIELNE -----
+POPRAWNY_LOGIN = "trenerpiotr@gigant.com"
+POPRAWNE_HASLO = "haslomaslo"
+
+# Prosze wczytać od użytkownika login i hasło, a następnie
+# zweryfikować czy podane dane są poprawne.
