bd1da737ae247f36f6bc2baaf4fcb9defcfaa94d
PiotrPolański <ppolanski@student.agh.edu.pl>
2025-11-10 15:19:39 +0100

    zadanie samodzielne L8

---
+++ Lekcja08/pokazowe.py
@@ -0,0 +1,13 @@
+tekst = "Kebabopoduszka"
+print(len(tekst))
+
+# exit()
+
+for litera in tekst: # Dla każdej LITERY z TEKSTU
+    print(litera, end = "HAHA")    # wykonaj COŚ
+
+
+# ZADANIE SAMODZIELNE
+
+# Proszę napisać program z pętlą for, który wypisze zawartość zmiennej tekst
+# (możemy tam wstawić dowolny tekst) oddzielając każdą literę znakiem spacji
