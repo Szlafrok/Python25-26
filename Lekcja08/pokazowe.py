0084747727231aae3caad60183e0be4997d61ea9
PiotrPolański <ppolanski@student.agh.edu.pl>
2025-11-10 15:35:47 +0100

    aktualizacja na prosbę L8

--- Lekcja08/pokazowe.py
+++ Lekcja08/pokazowe.py
@@ -4,10 +4,19 @@ print(len(tekst))
 # exit()
 
 for litera in tekst: # Dla każdej LITERY z TEKSTU
-    print(litera, end = "HAHA")    # wykonaj COŚ
+    print(litera, end = "__")    # wykonaj COŚ
 
 
 # ZADANIE SAMODZIELNE
 
 # Proszę napisać program z pętlą for, który wypisze zawartość zmiennej tekst
 # (możemy tam wstawić dowolny tekst) oddzielając każdą literę znakiem spacji
+print()
+for litera in tekst:
+    print(litera, end = " ")
+
+print()
+for i in range(8):
+    print(i, end = " ")
+
+
