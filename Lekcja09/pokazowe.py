a61ac44db0f773717241f763b4c41e0aef868ffc
PiotrekP <kamiennygigant@gmail.com>
2025-11-17 15:09:50 +0100

    zadanie rozgrzewkowe

---
+++ Lekcja09/pokazowe.py
@@ -0,0 +1,10 @@
+tekst = "12345"
+for znak in tekst:
+    print(znak)
+
+# Proszę napisać program, który wczyta od użytkownika
+# i zapisze do zmiennych 4 oceny z dowolnych przedmiotów
+# (np. WF, historia, polski, matematyka) a następnie wyliczy
+# średnią tych przedmiotów.
+
+# Średnia ocen to suma wszystkich ocen podzielona przez ich liczbę.
