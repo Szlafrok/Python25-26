2f35b9d15c46774b7a53cbeef1cbfa1b138b9148
PiotrekP <kamiennygigant@gmail.com>
2025-11-24 15:53:35 +0100

    start funkcji

---
+++ Lekcja10/pokaz.py
@@ -0,0 +1,18 @@
+lista1 = [2, 4, 5]
+lista2 = [6, 7, 8, 9]
+lista3 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+liczba = 0
+for elem in lista1:
+    liczba += elem
+print(liczba)
+
+liczba = 0
+for elem in lista2:
+    liczba += elem
+print(liczba)
+
+liczba = 0
+for elem in lista3:
+    liczba += elem
+print(liczba)
