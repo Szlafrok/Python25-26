0f710c16d07c43521378e2b3c5e073520a6f29fa
PiotrekP <kamiennygigant@gmail.com>
2025-11-24 16:17:04 +0100

    funkcja sumująca

--- Lekcja10/pokaz.py
+++ Lekcja10/pokaz.py
@@ -1,18 +1,29 @@
+def hejka():
+    print("Hejka!", end = " ")
+    print("Kaczkowróbel je kebaba i zabił nim gołombiodziobaka")
+    print("bo miał jajo z roleksem")
+
+hejka()
+
+def dodaj(a, b):
+    print(a + b)
+
+dodaj(5, 8)
+dodaj(11, 9)
+
+def sumuj_elementy(lista):
+    wynik = 0
+    for elem in lista:
+        wynik += elem
+    print(wynik)
+
 lista1 = [2, 4, 5]
 lista2 = [6, 7, 8, 9]
 lista3 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
 
-liczba = 0
-for elem in lista1:
-    liczba += elem
-print(liczba)
+sumuj_elementy(lista1)
 
-liczba = 0
-for elem in lista2:
-    liczba += elem
-print(liczba)
+sumuj_elementy(lista2)
 
-liczba = 0
-for elem in lista3:
-    liczba += elem
-print(liczba)
+sumuj_elementy(lista3)
+
