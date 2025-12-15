edcb32f518e6b032d15a0d05561edcbe00c1c16d
PiotrekP <kamiennygigant@gmail.com>
2025-12-15 16:45:27 +0100

    projekt gotowy

--- Lekcja13/projekt.py
+++ Lekcja13/projekt.py
@@ -14,6 +14,25 @@ def print_image(img_data):
     screen_surface.blit(surface, rect)
     return
 
+def set_position_image(img_data, position):
+    image, surface, rect = img_data
+    rect = surface.get_rect(center = position)
+    return [image, surface, rect]
+
+def calculate_movement(keys):
+    speed = 10
+    delta_x = 0
+    delta_y = 0
+    if keys[pygame.K_w]:
+        delta_y -= speed
+    if keys[pygame.K_s]:
+        delta_y += speed
+    if keys[pygame.K_d]:
+        delta_x += speed
+    if keys[pygame.K_a]:
+        delta_x -= speed
+    return [delta_x, delta_y]
+
 import pygame
 pygame.init()
 
@@ -25,6 +44,7 @@ pygame.display.set_caption("Pierwsza gra")
 
 player_pos = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 player = load_image("Lekcja13/assets/player.png", player_pos)
+background_color = [9, 42, 121]
 
 game_status = True
 
@@ -38,10 +58,17 @@ while game_status:
         if event.type == pygame.QUIT:
             game_status = False
         elif event.type == pygame.KEYDOWN:
+
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE]:
                 game_status = False
 
+            delta_x, delta_y = calculate_movement(keys)
+            player_pos[0] += delta_x
+            player_pos[1] += delta_y
+            player = set_position_image(player, player_pos)
+
+    screen_surface.fill(background_color) 
     print_image(player)
 
     pygame.display.update()
