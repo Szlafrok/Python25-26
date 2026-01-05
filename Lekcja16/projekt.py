2618f32e7502d06b3f96e549eebdab1ce553f77e
PiotrekP <kamiennygigant@gmail.com>
2026-01-05 16:19:24 +0100

    wypłata pieniędzy

--- Lekcja16/projekt.py
+++ Lekcja16/projekt.py
@@ -8,23 +8,39 @@ def glowne_menu():
     # Proszę uzupełnić tę funkcję w taki sposób, aby wypisywała ona pytanie do użytkownika
     # co chce zrobić oraz opcje 1-4: Wypłata, wpłata, sprawdzenie stanu konta, zakończenie aplikacji.
 
-def wczytaj_dane() -> int:
-    return int(input("Wprowadź wybór: "))
+def wczytaj_dane(zapytanie) -> int:
+    return int(input(zapytanie))
     # Proszę uzupełnić tę funkcję w ten sposób, aby zwracała wartość opcji wpisanej przez użytkownika
+
+def wypisz_stan_konta(saldo):
+    print(f"Aktualny stan konta: {saldo} zł")
+    # Proszę wypisać aktualny stan konta w formacie: "Aktualny stan konta: XX zł"
+    # Proszę zastosować format f-string: f"{}"
+
+def wyplata_pieniedzy(saldo, wartosc): # Funkcja przyjmuje argumenty i zwraca NOWE SALDO
+    if wartosc > saldo:
+        print("Nie możesz tyle wypłacić - za mało pieniędzy!")
+        return saldo
+    print(f"Wypłacono {wartosc} zł!")
+    nowe_saldo = saldo - wartosc
+    wypisz_stan_konta(nowe_saldo)
+    return nowe_saldo
+
 
 wybor = 0
 saldo = 0
 
 while True:
     glowne_menu()
-    wybor = wczytaj_dane()
+    wybor = wczytaj_dane("Podaj wybór: ")
 
     if wybor == 1:
         pass
     elif wybor == 2:
-        pass
+        wartosc = wczytaj_dane("Podaj wartość: ")
+        wyplata_pieniedzy(saldo, wartosc)
     elif wybor == 3:
-        pass
+        wypisz_stan_konta(saldo)
     elif wybor == 4:
         print("Aplikacja została zamknięta!")
         break
