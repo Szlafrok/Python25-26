42f003f51f1c7f8b8e0c0461904f51b662c0d424
PiotrekP <kamiennygigant@gmail.com>
2026-01-12 15:32:58 +0100

    L17 so far so good

---
+++ Lekcja17/projekt.py
@@ -0,0 +1,23 @@
+zero = ["0", "zero", "zera", "zerem"]
+jeden = ["1", "jeden", "jedynkę", "jedynka", "jednego", "jedynką"]
+plus = ["+", "dodaj", "dodawanie", "dodać", "plus"] # tego komentarza już w liście nie ma
+
+baza = [zero, jeden, plus]
+
+# print(baza[2])
+# plus.append("dodawaj")
+# print(baza[2])
+
+dzialanie = ""
+tekst = input("Podaj treść działania: ")
+
+print(tekst)
+print(tekst.split(" "))
+
+def przetlumacz_slowo(slowo):
+    for wyrazenie in baza:
+        if slowo in wyrazenie:
+            return wyrazenie[0]
+
+for slowo in tekst.split(" "):
+    pass
