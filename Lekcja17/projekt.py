bc8ca15c9f1979683b85336d1b76fd77e5c19b4e
PiotrekP <kamiennygigant@gmail.com>
2026-01-12 16:37:39 +0100

    gotowy projekt

--- Lekcja17/projekt.py
+++ Lekcja17/projekt.py
@@ -1,8 +1,38 @@
 zero = ["0", "zero", "zera", "zerem"]
-jeden = ["1", "jeden", "jedynkę", "jedynka", "jednego", "jedynką"]
-plus = ["+", "dodaj", "dodawanie", "dodać", "plus"] # tego komentarza już w liście nie ma
+jeden = ["1", "jeden", "jedynka", "jednego", "jednej", "jedynkę", "jedynką"]
+dwa = ["2", "dwa", "dwójka", "dwóch", "dwie", "dwójkę", "dwójką"]
+trzy = ["3", "trzy", "trójka", "trzech", "trójkę", "trójką"]
+cztery = ["4", "cztery", "czwórka", "czterech", "czwórkę", "czwórką"]
+piec = ["5", "pięć", "piątka", "pięciu", "piątkę", "piątką"]
+szesc = ["6", "sześć", "szóstka", "sześciu", "szóstkę", "szóstką"]
+siedem = ["7", "siedem", "siódemka", "siedmiu", "siódemkę", "siódemką"]
+osiem = ["8", "osiem", "ósemka", "ośmiu", "ósemkę", "ósemką"]
+dziewiec = ["9", "dziewięć", "dziewiątka", "dziewięciu", "dziewiątkę", "dziewiątką"]
+dziesiec = ["10", "dziesięć", "dziesiątka", "dziesięciu", "dziesiątkę", "dziesiątką"]
+jedenascie = ["11", "jedenaście", "jedenastka", "jedenastu"]
+dwanascie = ["12", "dwanaście", "dwunastka", "dwunastu"]
+trzynascie = ["13", "trzynaście", "trzynastka", "trzynastu"]
+czternascie = ["14", "czternaście", "czternastka", "czternastu"]
+pietnascie = ["15", "piętnaście", "piętnastka", "piętnastu"]
+szesnascie = ["16", "szesnaście", "szesnastka", "szesnastu"]
+siedemnascie = ["17", "siedemnaście", "siedemnastka", "siedemnastu"]
+osiemnascie = ["18", "osiemnaście", "osiemnastka", "osiemnastu"]
+dziewietnascie = ["19", "dziewiętnaście", "dziewiętnastka", "dziewiętnastu"]
+dwadziescia = ["20", "dwadzieścia", "dwudziestka", "dwudziestu"]
 
-baza = [zero, jeden, plus]
+plus = ["+", "plus", "dodaj", "dodać", "dodawanie", "suma"]
+minus = ["-", "minus", "odejmij", "odjąć", "odejmowanie", "różnica"]
+razy = ["*", "x", "razy", "pomnóż", "pomnożyć", "mnożenie", "iloczyn"]
+podziel = ["/", "÷", "podziel", "podzielić", "dzielenie", "iloraz"]
+
+baza = [
+    zero, jeden, dwa, trzy, cztery, piec, szesc, siedem, osiem, dziewiec,
+    dziesiec, jedenascie, dwanascie, trzynascie, czternascie,
+    pietnascie, szesnascie, siedemnascie, osiemnascie,
+    dziewietnascie, dwadziescia,
+    plus, minus, razy, podziel
+]
+
 
 # print(baza[2])
 # plus.append("dodawaj")
@@ -57,4 +87,5 @@ def oblicz_z_tekstu(tekst: str):
 for slowo in tekst.split(" "):
     dzialanie += przetlumacz_slowo(slowo)
 
+print(dzialanie)
 print(oblicz_z_tekstu(dzialanie))
