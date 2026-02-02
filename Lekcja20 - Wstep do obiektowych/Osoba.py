961f0255cfd14c3acb98fb4fdeec0939a4155b1a
PiotrekP <kamiennygigant@gmail.com>
2026-02-02 15:49:23 +0100

    przed przerwą

--- Lekcja20 - Wstep do obiektowych/Osoba.py
+++ Lekcja20 - Wstep do obiektowych/Osoba.py
@@ -2,3 +2,6 @@ class Osoba():
     imie = ""
     nazwisko = ""
     wiek = 0
+
+    def dane(self):
+        return f"Jestem {self.imie} {self.nazwisko} i mam {self.wiek} lat."
--- Lekcja20 - Wstep do obiektowych/przyklad.py
+++ Lekcja20 - Wstep do obiektowych/przyklad.py
@@ -1,21 +1,9 @@
-imie1 = "Grzegorz"
-nazwisko1 = "Brzęczyszczykiewicz"
-wiek1 = 69
+from Osoba import Osoba
 
-imie2 = "Amir"
-nazwisko2 = "Wójcicki"
-wiek2 = 21
+person = Osoba()
 
-imie3 = "Jan"
-nazwisko3 = "Dudek"
-wiek3 = 40
+person.imie = "Jan"
+person.nazwisko = "Kowalski"
+person.wiek = 18
 
-imie4 = "Ryszard"
-nazwisko4 = "Rabarbarowicz"
-wiek4 = 60
-
-print(imie1, nazwisko1)
-if (wiek1 >= 18):
-    print(f"{imie1} jest pełnoletni")
-else:
-    print(f"{imie1} nie jest pełnoletni")
+print(person.dane())
