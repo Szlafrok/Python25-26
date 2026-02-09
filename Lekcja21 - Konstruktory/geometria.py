346307ef17a1527b173013620596d7ddaa6b5021
PiotrekP <kamiennygigant@gmail.com>
2026-02-09 16:16:37 +0100

    geometria

--- Lekcja21 - Konstruktory/geometria.py
+++ Lekcja21 - Konstruktory/geometria.py
@@ -27,4 +27,32 @@ print(trojkat.pole())
 # Wzór na obwód koła: 2 * pi * r
 # Wzór na pole koła: pi * r**2
 
+class Prostokat():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def obwod(self):
+        return self.a * 2 + self.b * 2 # 2 * (self.a + self.b)
+    
+    def pole(self):
+        return self.a * self.b
+    
 from math import pi
+class Kolo():
+    def __init__(self, promien):
+        self.promien = promien
+
+    def obwod(self):
+        return 2 * pi * self.promien
+    
+    def pole(self):
+        return pi * self.promien**2
+    
+figura1 = Prostokat(3, 5)
+print(figura1.obwod())
+print(figura1.pole())
+
+figura2 = Kolo(5)
+print(figura2.obwod())
+print(figura2.pole())
