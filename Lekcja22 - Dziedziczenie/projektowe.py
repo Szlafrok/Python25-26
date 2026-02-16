d016d1661231ab384972be56a4d10daa678d2a58
PiotrekP <kamiennygigant@gmail.com>
2026-02-16 16:36:20 +0100

    koniec lekcji 22

--- Lekcja22 - Dziedziczenie/projektowe.py
+++ Lekcja22 - Dziedziczenie/projektowe.py
@@ -31,13 +31,38 @@ class Kot(Zwierze):
     def miau(self):
         print("≽^•⩊•^≼")
 
+class Ptak(Zwierze):
+    def __init__(self, imie, wiek, predkoscLotu):
+        super().__init__(imie, wiek)
+        self.predkoscLotu = predkoscLotu
 
-# Klasa kot dziedzicząca po Zwierzę
-# Kot posiada dodatkowo swoje umaszczenie (barwę sierści)
-# Potrafi wypisać to umaszczenie
+    def wypiszPredkosc(self):
+        print(f"{self.imie} lata z prędkością {self.predkoscLotu} km/h")
+
+
+class Orzel(Ptak):
+    def __init__(self, imie, wiek, predkoscLotu):
+        super().__init__(imie, wiek, predkoscLotu)
+
+    def lecisz(self):
+        print(f"{self.imie} POTĘŻNY PRZELOT ORŁA z prędkością {self.predkoscLotu}")
+
+
+# [+++] Proszę napisać klasę Ptak, która dziedziczy po klasie Zwierze.
+#       Ptak dodatkowo posiada swoją prędkość lotu wyrażoną w liczbie i potrafi wypisać tę prędkość.
+
+# [++] [Można zrobić, ale potem zrobimy to wspólnie] Proszę napisać klasę Orzel, która
+#      dziedziczy po klasie Ptak. Ma takie same parametry, ale dodatkowo ma metodę lecisz(), która
+#      wypisuje "POTĘŻNY PRZELOT ORŁA"
+
+# [+] Proszę przetestować napisane klasy.
 
 zwierz1 = Pies("Azor", 11, "Labrador")
 zwierz2 = Kot("Andrzej", 16, "Rude")
+zwierz3 = Ptak("Leszek", 10, 30)
+
+zwierz3.wypiszPredkosc()
+zwierz3.wydajDzwiek()
 
 zwierz1.wydajDzwiek()
 zwierz2.wydajDzwiek()
@@ -46,3 +71,6 @@ zwierz1.podajRase()
 
 zwierz2.podajUmaszczenie()
 zwierz2.miau()
+
+orzel = Orzel("Michał", 200, 1500)
+orzel.lecisz()
