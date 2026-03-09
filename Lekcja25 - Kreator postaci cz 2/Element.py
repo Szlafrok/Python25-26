8dfe785b3ab7b1e1267ddd97644fbce08c579213
PiotrekP <kamiennygigant@gmail.com>
2026-03-09 15:43:18 +0100

    przed przerwą

--- Lekcja25 - Kreator postaci cz 2/Element.py
+++ Lekcja25 - Kreator postaci cz 2/Element.py
@@ -1,3 +1,18 @@
 import pygame
+from setup import IMAGE_PATH
 
-class Obraz(pygame.sprite.Sprite)
+class Image(pygame.sprite.Sprite):
+    def __init__(self, path: str):
+        super().__init__()
+        self.image = pygame.image.load(path)
+
+class Element():
+    def __init__(self, elem_type):
+        self.selected = 0 # wybrany
+
+        self.image_list = [] # lista obrazów
+
+        for i in range(1, 4):
+            path = f"{IMAGE_PATH}{elem_type}{i}.png"
+            loaded_image = Image(path)
+            self.image_list.append(loaded_image)
