0d733cb6b91a1fd47bc1922e215051c81cc879aa
PiotrekP <kamiennygigant@gmail.com>
2026-04-20 16:35:59 +0200

    kulka

---
+++ Lekcja30 - Arkanoid cz 2/ball.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+from setup import *
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(f"{IMAGE_PATH}/ball.png")
+        self.reset_position()
+        self.r = 16
+        self.lost = False
+
+
+    def reset_position(self):
+        self.coordinates = pygame.math.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 120)
+        self.position = self.image.get_rect(center = self.coordinates)
+
+        self.vector = pygame.math.Vector2(0, -10)
+        self.angle = random.randint(-30, 30)
+        self.vector.rotate_ip(self.angle)
+
+        self.lost = False
+
+
+    def update(self, pad):
+        self.coordinates += self.vector
+        self.position.center = self.coordinates
