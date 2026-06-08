82a4109e07b954beadf8d59857f467f51d92244b
PiotrekP <kamiennygigant@gmail.com>
2026-06-08 15:32:32 +0200

    krotki

---
+++ Lekcja36 - Struktury danych/krotka.py
@@ -0,0 +1,20 @@
+krotka = (4, 2, 11, 2, 4)
+
+print(krotka[0])
+print(len(krotka))
+
+print(krotka[-2])
+print(krotka[-5])
+print(krotka[4])
+
+jednoelementowa = (5,)
+print(jednoelementowa)
+print(jednoelementowa[0])
+
+pusta = ()
+print(type(pusta))
+
+
+print(krotka.count(4))
+
+print(krotka.index(4))
