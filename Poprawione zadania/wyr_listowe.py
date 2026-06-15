f9c239532782cb040cde0b02724c8187daa85b1c
PiotrekP <kamiennygigant@gmail.com>
2026-06-15 15:52:01 +0200

    wyrażenia listowe

--- Poprawione zadania/wyr_listowe.py
+++ Poprawione zadania/wyr_listowe.py
@@ -11,3 +11,25 @@ print(lista)
 # wszystkich liczb PARZYSTYCH od 1 do 50. (++)
 
 # [*] (+) Proszę zapisać liczby w liście w kolejności malejącej
+
+lista = [x*2 for x in range(1, 26)]
+lista = [x for x in range(50, 1, -2)]
+
+lista = [x for x in range(1, 51) if x % 2 == 0]
+               # 1. Wyciągam
+                                 # 2. Sprawdzam
+        # 3. Zamieszczam
+
+
+print(lista) 
+
+
+slowa = ["kebab", "ta", "awupa", "piłkarzyki", "nic", "płakał", "herobrine"]
+
+print([slowo for slowo in slowa])
+
+print(["aaa" + slowo for slowo in slowa])
+
+print([slowo for slowo in slowa if len(slowo) > 5])
+
+print(["aaa" + slowo for slowo in slowa if len(slowo) > 5])
