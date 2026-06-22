52f13d101e05784dd743f58fe3d605cd59d68644
PiotrekP <kamiennygigant@gmail.com>
2026-06-22 16:23:04 +0200

    aktualizacja

--- Lekcja38 - Generowanie PDF/pokaz.py
+++ Lekcja38 - Generowanie PDF/pokaz.py
@@ -1,2 +1,13 @@
-import fpdf
+file_path = "Lekcja38 - Generowanie PDF"
 
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.add_page()
+
+#             rodzina styl ścieżka unikodowe
+pdf.add_font("DejaVu", "", f"{file_path}/assets/DejaVuSansCondensed.ttf", uni=True)
+pdf.set_font("DejaVu", size = 12)
+pdf.cell(text = "Oferta biura podróży")
+
+pdf.output(f"{file_path}/OfertaGotowa.pdf")
